3e1830218a71cd4c5a48e19100e3b7acae04aa42
EstebanEslava <estebancalishop1210@gmail.com>
2025-03-04 22:28:46 -0500

    Proyecto listo

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ let openedPopup = document.getElementById("popup");
 let ButtonOpened = document.getElementById("button-opened");
 let ButtonClose = document.getElementById("button-close");
 let ButtonSubmit = document.getElementById("submit");
+const submitImage = document.getElementById("submit-image");
 const InputName = document.getElementById("input-name");
 const inputOccupation = document.getElementById("input-occupation");
 const NameProfile = document.querySelector(".profile__title");
@@ -15,9 +16,9 @@ const openedPopupAdd = document.getElementById("popup-add");
 const ButtonOpenedAdd = document.getElementById("button-openedAdd");
 const ButtonCloseAdd = document.getElementById("button-closeAdd");
 let popuptext = document.querySelector("#popup-text");
-const popupimage = document.querySelector("#popup-image");
+const popupImage = document.querySelector("#popup-image");
 const namePopup = document.querySelector("#name-popup");
-
+const ButtonCloseImage = document.getElementById("button-close-image");
 const initialCards = [
   {
     name: "Valle de Yosemite",
@@ -44,50 +45,52 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
   },
 ];
-const cardList = document.querySelector(".elements");
 const cardTemplate = document.querySelector("#element-template");
 
 initialCards.forEach((card) => {
+  addCard(card);
+});
+function addCard(card) {
   const newCard = cardTemplate.content
     .querySelector(".element")
     .cloneNode(true);
-
+  const cardList = document.querySelector(".elements");
   const imageCard = newCard.querySelector(".element__image");
   const nameCardElement = newCard.querySelector(".element__text");
-  //const buttonLike = newCard.querySelector("#button-like");
-  //const removeCard = newCard.querySelector(".element-remove__card");
+  const buttonLike = newCard.querySelector("#button-like");
+  const removeCard = newCard.querySelector("#remover-card");
   imageCard.src = card.link;
   nameCardElement.textContent = card.name;
-  InputNameCard.src = nameCardElement.name;
-  inputImageUrl.textContent = imageCard.link;
+  InputNameCard.textContent = nameCardElement.name;
+  inputImageUrl.src = imageCard.link;
+  removeCard.addEventListener("click", () => {
+    newCard.remove();
+  });
+
+  buttonLike.addEventListener("click", (evt) => {
+    evt.target.classList.toggle("element__button-like_active");
+  });
+
+  imageCard.addEventListener("click", () => {
+    imagePopup.classList.toggle("popup_opened");
+
+    popupImage.src = card.link;
+    namePopup.textContent = card.name;
+  });
 
   cardList.prepend(newCard);
+}
+
+ButtonCloseImage.addEventListener("click", function () {
+  imagePopup.classList.remove("popup_opened");
 });
+
 ButtonOpenedAdd.addEventListener("click", function () {
   openedPopupAdd.classList.add("popup_opened");
 });
 
 ButtonCloseAdd.addEventListener("click", function () {
-  openedPopupAdd.classList.remove("popup-add");
-});
-
-ButtonSubmit.addEventListener("click", (evt) => {
-  evt.preventDefault();
-});
-
-removeCard.addEventListener("click", () => {
-  newCard.remove();
-});
-
-buttonLike.addEventListener("click", () => {
-  buttonLike.classList("");
-});
-
-imageCard.addEventListener("click", () => {
-  imagePopup.showModal();
-
-  popupimage.src = card.link;
-  namePopup.textContent = card.name;
+  openedPopupAdd.classList.remove("popup_opened");
 });
 
 ButtonOpened.addEventListener("click", function () {
@@ -95,9 +98,23 @@ ButtonOpened.addEventListener("click", function () {
 });
 
 ButtonClose.addEventListener("click", function () {
-  openedPopup.classList.remove("popup");
+  openedPopup.classList.remove("popup_opened");
 });
 
+submitImage.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  const InputNameCard = document.getElementById("name-title").value;
+  const inputImageUrl = document.getElementById("image-url").value;
+
+  const card = {
+    name: InputNameCard,
+    link: inputImageUrl,
+  };
+
+  addCard(card);
+
+  openedPopupAdd.classList.remove("popup_opened");
+});
 ButtonSubmit.addEventListener("click", function (evt) {
   evt.preventDefault();
 
@@ -107,5 +124,5 @@ ButtonSubmit.addEventListener("click", function (evt) {
   NameProfile.textContent = NameValue;
   Occupation.textContent = OccupationValue;
 
-  openedPopup.classList.remove("popup");
+  openedPopup.classList.remove("popup_opened");
 });
